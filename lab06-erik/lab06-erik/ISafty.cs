7aca24cc4fcf2a38131f64518873b3d2c4f42d8a
drkrieger <eplyushko@gmail.com>
2017-09-19 12:44:31 -0700

    lab6 build out

---
+++ lab06-erik/lab06-erik/ISafty.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace lab06_erik
+{
+    interface ISafty
+    {
+    }
+}
---
+++ lab06-erik/lab06-erik/MountainBike.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace lab06_erik
+{
+    class MountainBike : Bike, IRide
+    {
+        public void Ride(IRide person)
+        {
+            person.Peddel();
+        }
+    }
+}
