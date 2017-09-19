9a037bb6cd4ec0c6fa6228301cc3d8ed65ab0b6b
Brandon Little <brlittle86@gmail.com>
2017-09-19 10:19:21 -0700

    implemented interfaces for Warrior class and made constructor method

--- lab06-brandon/GameJobs/GameJobs/Warrior.cs
+++ lab06-brandon/GameJobs/GameJobs/Warrior.cs
@@ -4,8 +4,16 @@ using System.Text;
 
 namespace GameJobs
 {
-    class Warrior : Player
+    class Warrior : Player, IOneHandedWeapons, ITwoHandedWeapons
     {
+        public Warrior(string name, int maxHp, int defense)
+        {
+            Name = name;
+            MaxHP = maxHp;
+            CurrentHP = maxHp;
+            Defense = defense;
+        }
+
         public void IsStrong()
         {
             Console.WriteLine($"{Name} is strong and tough.");
@@ -14,6 +22,26 @@ namespace GameJobs
         public override void Attack()
         {
             Console.WriteLine($"{Name} attacks with strength and skill.");
+        }
+
+        public void HasStrength()
+        {
+            Console.WriteLine($"{Name} can lift a horse.");
+        }
+
+        public void HasTwoHandedSkill()
+        {
+            Console.WriteLine($"{Name} swings two-handed weapons like a boss.");
+        }
+
+        public void HasDexterity()
+        {
+            Console.WriteLine($"{Name} can delicately handle small things.");
+        }
+
+        public void HasOneHandedSkill()
+        {
+            Console.WriteLine($"{Name} is a master of the single blade.");
         }
     }
 }
