007a8b0784424b9a445a71bfcd9dd7c49f1f4346
Thomas Reyes <thomasellison.r@gmail.com>
2017-09-19 10:32:13 -0700

    Lab finished, README updated, diagram included

---
+++ lab06-tom/lab06tom/Defense.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace lab06tom
+{
+    class Defense : Character, IShoot, IAbilities
+    {
+        public Defense(string name, int age) : base(name, age)
+        {
+        }
+
+        public void AbilityOne()
+        {
+            Console.WriteLine("Defense characters have an Ability 1");
+        }
+
+        public void AbilityTwo()
+        {
+            Console.WriteLine("Defense characters have an Ability 2");
+
+        }
+
+        public void Aim()
+        {
+            Console.WriteLine("Target acquired!");
+        }
+
+        public void Fire()
+        {
+            Console.WriteLine("Light them up!");
+        }
+
+        public void Primary()
+        {
+            Console.WriteLine("Defense characters have a Primary Fire");
+        }
+
+        public void Reload()
+        {
+            Console.WriteLine("Out of ammo!");
+        }
+
+        public void Ultimate()
+        {
+            Console.WriteLine("Defense characters have an Ultimate Ability");
+        }
+    }
+}
