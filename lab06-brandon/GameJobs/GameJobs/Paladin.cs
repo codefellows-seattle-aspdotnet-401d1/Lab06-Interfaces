b52edf81204241bc891aab15ba2dbfbd91ac2c0f
Brandon Little <brlittle86@gmail.com>
2017-09-19 10:09:53 -0700

    implemented interfaces into Paladin

--- lab06-brandon/GameJobs/GameJobs/Paladin.cs
+++ lab06-brandon/GameJobs/GameJobs/Paladin.cs
@@ -4,7 +4,7 @@ using System.Text;
 
 namespace GameJobs
 {
-    class Paladin : Player
+    class Paladin : Player, IOneHandedWeapons, ITwoHandedWeapons, ISpellcasting
     {
         public void IsDivine()
         {
@@ -14,6 +14,36 @@ namespace GameJobs
         public override void Attack()
         {
             Console.WriteLine($"{Name} attacks with righteous fury.");
+        }
+
+        public void HasKnowledge()
+        {
+            Console.WriteLine($"{Name} knows much about the gods.");
+        }
+
+        public void HasIntelligence()
+        {
+            Console.WriteLine($"{Name} has read some books.");
+        }
+
+        public void HasStrength()
+        {
+            Console.WriteLine($"{Name} can lift a person with ease.");
+        }
+
+        public void HasTwoHandedSkill()
+        {
+            Console.WriteLine($"{Name} can swing two-handed weapons with ease and skill.");
+        }
+
+        public void HasDexterity()
+        {
+            Console.WriteLine($"{Name} won't get their fingers twisted up doing fine work.");
+        }
+
+        public void HasOneHandedSkill()
+        {
+            Console.WriteLine($"{Name} is an expert duelist.");
         }
     }
 }
