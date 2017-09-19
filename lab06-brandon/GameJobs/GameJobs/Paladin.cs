c8cad4240e7ac03a7726d1bcd9aed4fa840f4122
Brandon Little <brlittle86@gmail.com>
2017-09-19 09:53:10 -0700

    filled out the Paladin class

--- lab06-brandon/GameJobs/GameJobs/Paladin.cs
+++ lab06-brandon/GameJobs/GameJobs/Paladin.cs
@@ -4,7 +4,16 @@ using System.Text;
 
 namespace GameJobs
 {
-    class Paladin
+    class Paladin : Player
     {
+        public void IsDivine()
+        {
+            Console.WriteLine($"{Name} is full of holy power.");
+        }
+
+        public override void Attack()
+        {
+            Console.WriteLine($"{Name} attacks with righteous fury.");
+        }
     }
 }
