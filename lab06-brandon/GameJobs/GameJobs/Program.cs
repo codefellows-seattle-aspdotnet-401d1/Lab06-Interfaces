1b7583a4a04f3b2fa66cceb6d5309fed5d8c05fe
Brandon Little <brlittle86@gmail.com>
2017-09-19 10:49:30 -0700

    created instances of the four classes, created some methods that require certain interfaces, and called them.

--- lab06-brandon/GameJobs/GameJobs/Program.cs
+++ lab06-brandon/GameJobs/GameJobs/Program.cs
@@ -6,7 +6,24 @@ namespace GameJobs
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Mage john = new Mage("John", 6, 5);
+            Paladin enya = new Paladin("Enya", 9, 10);
+            Priest bob = new Priest("Bob", 7, 6);
+            Warrior christina = new Warrior("Christina", 12, 12);
+
+            Riposte(enya);
+            Riposte(christina);
+            Fireball(john);
+        }
+
+        static void Riposte(IOneHandedWeapons duelist)
+        {
+            Console.WriteLine("Only a skilled combatant can pull off a riposte!");
+        }
+
+        static void Fireball(ISpellcasting caster)
+        {
+            Console.WriteLine("Try creating a ball of fire from nothing without learning to cast spells. I dare you.");
         }
     }
 }
