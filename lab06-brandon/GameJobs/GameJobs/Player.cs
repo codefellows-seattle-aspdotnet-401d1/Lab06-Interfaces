7618fd6d020cad7369c8697689d7b6891ba6292a
Brandon Little <brlittle86@gmail.com>
2017-09-19 09:47:50 -0700

    created class and interface files, defined Player class

---
+++ lab06-brandon/GameJobs/GameJobs/Player.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GameJobs
+{
+    abstract class Player
+    {
+        public string Name { get; set; }
+        public int MaxHP { get; set; }
+        public int CurrentHP { get; set; }
+        public int Defense { get; set; }
+
+        public abstract void Attack();
+    }
+}
