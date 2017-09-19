44a26379a9c305ab4fb0c477058a36d495c45556
Brandon Little <brlittle86@gmail.com>
2017-09-19 09:55:27 -0700

    filled out the Mage class

--- lab06-brandon/GameJobs/GameJobs/Mage.cs
+++ lab06-brandon/GameJobs/GameJobs/Mage.cs
@@ -4,7 +4,17 @@ using System.Text;
 
 namespace GameJobs
 {
-    class Mage
+    class Mage : Player
     {
+
+        public void IsMystical()
+        {
+            Console.WriteLine($"{Name} vibrates with mystical power.");
+        }
+
+        public override void Attack()
+        {
+            Console.WriteLine($"{Name} attacks with arcane energy.");
+        }
     }
 }
